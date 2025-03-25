c113c1efd7bc460efbc54cf027ea9df69a7c69dc
kylejchiu <160537581+kylejchiu@users.noreply.github.com>
2025-03-25 20:30:35 +0000

    Data Types

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
 print("Hello World")
+age = 15
+name = "Kyle"
+favorite_color = "Teal"
+string = "Hello!"
+integer = 1000
+float = 0.5
+boolean = True
+length = int(input("Please enter the length: "))
+width = int(input("Please enter the width: "))
+area = width * length
+print(area)
